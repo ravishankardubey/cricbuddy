f89ee5a73de4e1a1c2701049ff41e565a42ae8e2
Ravi Shankar Dubey <ravishankar.rsd@gmail.com>
2018-07-26 23:49:28 +0530

    nav-header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavHeaderComponent } from './components/nav-header/nav-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule
