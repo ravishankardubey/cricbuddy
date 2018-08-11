0971c0f45d6e6b90266224c55ac209f0739fc5d7
Ravi Shankar Dubey <ravishankar.rsd@gmail.com>
2018-08-11 21:33:08 +0530

    Angular CLI Upgraded From 4 -> 6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 @NgModule({
   exports: [RouterModule],
   imports: [
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes, { useHash: true, scrollPositionRestoration: 'enabled' })
   ]
 })
 
